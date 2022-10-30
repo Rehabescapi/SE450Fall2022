2fbcf6ace3b2d43a7bdb536189b23dd8c6647f33
Vahid Alizadeh <vahid.alizadeh@gmail.com>
2022-10-30 18:47:50 -0500

    HW4

---
+++ src/main/java/assignment4/problem/Converter.java
@@ -0,0 +1,25 @@
+/*
+ *
+ *  * Copyright (c) 2022.
+ *  * Vahid Alizadeh
+ *  * Object-oriented Software Development
+ *  * DePaul University
+ *
+ */
+
+package assignment4.problem;
+
+public class Converter {
+
+
+	public static void main(String[] args) {
+		ConverterGUI ui = new ConverterGUI();
+
+	}
+
+	//You can add method (s) if necessary here or in a separate class to implement the converter logic using the patterns
+	//static Convert(..) method >> this is where you will initialize the pattern and call the appropriate method(s) to convert the input.
+	// Instantiating Handlers and setSuccessors?
+	//Decorators?
+	//You will call this method from the GUI class
+}
